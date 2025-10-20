0c9149e6a020906f103ad25369b5f2ad517288b3
GianniC88 <gianni.colucci.88@gmail.com>
2025-10-20 16:30:29 +0200

    Implementa la gestione degli errori in getDashboardData() utilizzando Promise.allSettled() e aggiungi la funzione fetchJson() per le chiamate API ( bonus 2 )

--- script.js
+++ script.js
@@ -132,6 +132,8 @@ Se l’array di ricerca è vuoto, invece di far fallire l'intera funzione, sempl
 Vienna is in Austria.
 The main airport is Vienna International Airport.
 */
+
+/*
 async function fetchJson(url) {
   const response = await fetch(url);
   const obj = await response.json();
@@ -200,7 +202,7 @@ getDashboardData('Vienna')
   })
   .catch(error => console.error(error));
 
-
+*/
 
 /*🎯 Bonus 2 - Chiamate fallite
 Attualmente, se una delle chiamate fallisce, Promise.all() rigetta l'intera operazione.
@@ -210,3 +212,93 @@ Se una chiamata fallisce, i dati relativi a quella chiamata verranno settati a n
 Stampa in console un messaggio di errore per ogni richiesta fallita.
 Testa la funzione con un link fittizio per il meteo (es. https://www.meteofittizio.it).
 */
+
+
+
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const obj = await response.json();
+  return obj;
+}
+
+async function getDashboardData(query) {
+
+
+
+  try {
+    console.log(`Il paese di destinazione è "${query}"`)
+
+    const destinationsPromise = fetchJson(` http://localhost:3333/destinations?search=${query}`);
+
+    const weathersPromise = fetchJson(`http://localhost:3333/weathers?search=${query}`);
+
+    const airportsPromise = fetchJson(` http://localhost:3333/airports?search=${query}`);
+
+    const promises = [destinationsPromise, weathersPromise, airportsPromise];
+    const [destinationsResult, weathersResult, airportsResult] = await Promise.allSettled(promises)
+
+    const data = {};
+
+    //controllo il risultato di destination
+
+    if (destinationsResult.status === 'rejected') {
+      console.error('Problem in destinations:', destinationsResult.reason);
+      data.city = null;
+      data.country = null;
+    } else {
+      const destination = destinationsResult.value[0];
+      data.city = destination?.name ?? null;
+      data.country = destination?.country ?? null;
+
+    }
+
+    //controllo risutati di weathers
+    if (weathersResult.status === 'rejected') {
+      console.error('Problem in weathers:', weathersResult.reason);
+      data.temperature = null;
+      data.weather = null;
+    } else {
+      const weather = weathersResult.value[0];
+      data.temperature = weather?.temperature ?? null;
+      data.weather = weather?.weather_description ?? null;
+
+    }
+
+    //controllo di dati di airports
+
+    if (airportsResult.status === 'rejected') {
+      console.error('Problem in airports:', airportsResult.reason);
+      data.airport = null;
+
+    } else {
+      const airport = airportsResult.value[0];
+      data.airport = airport?.name ?? null;
+
+
+    }
+    return data;
+
+  } catch (error) {
+    throw new Error(`Dati non trovati in : ${error.message}`)
+  }
+}
+
+
+getDashboardData('London')
+  .then(data => {
+    console.log('Dasboard data:', data);
+    let frase = "";
+    if (data.city !== null && data.country !== null) {
+      frase += `${data.city}is in ${data.country}.\n`;
+    }
+    if (data.temperature !== null && data.weather !== null) {
+      frase += `Today there are ${data.temperature} degrees and the weather is ${data.weather} .\n`;
+    }
+    if (data.airport !== null) {
+      frase += `the main ariport id ${data.airport}.\n`;
+    }
+
+    console.log(frase)
+  })
+  .catch(error => console.error(error));
+
